0451dbaabcad24c20e8baa2a8c91f845bdda13a1
Nils Hartmann <nils@nilshartmann.net>
2022-03-10 15:12:47 +0100

    youtube

--- src/main/java/nh/graphql/blogexample/graphql/BlogExampleController.java
+++ src/main/java/nh/graphql/blogexample/graphql/BlogExampleController.java
@@ -4,8 +4,6 @@ import nh.graphql.blogexample.domain.BlogService;
 import nh.graphql.blogexample.domain.Comment;
 import nh.graphql.blogexample.domain.Post;
 import nh.graphql.blogexample.domain.PostRepository;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
@@ -13,49 +11,37 @@ import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
-import java.util.Optional;
 
 @Controller
-public class BlogGraphQLController {
+public class BlogExampleController {
 
   private PostRepository postRepository;
   private BlogService blogService;
 
-
-  public BlogGraphQLController(PostRepository postRepository, BlogService blogService) {
+  public BlogExampleController(PostRepository postRepository, BlogService blogService) {
     this.postRepository = postRepository;
     this.blogService = blogService;
   }
 
   @QueryMapping
-  public List<Post> posts(@Argument Optional<Integer> size,
-                          @Argument Optional<Integer> page) {
-    Pageable pageable = PageRequest.of(
-      page.orElse(0),
-      size.orElse(10));
-    return postRepository.findAll(pageable).getContent();
+  public List<Post> posts() {
+    return postRepository.findAll();
   }
 
   @SchemaMapping
   public String excerpt(Post post, @Argument int maxLength) {
-    var body = post.getBody();
-    if (body.length()<maxLength) {
-      return body;
-    }
-
-    return body.substring(0, maxLength);
+    return post.getBody().substring(0, maxLength);
   }
 
-  @QueryMapping
-  public Optional<Post> post() {
-    return postRepository.findFirstByOrderByCreatedAtDesc();
-  }
-//
+  record NewCommentInput(String userId, String postId, String comment) {}
+
   @MutationMapping
   public Comment newComment(@Argument NewCommentInput input) {
-    return blogService.addComment(
+    return this.blogService.addComment(
       input.userId(),
       input.postId(),
-      input.comment());
+      input.comment
+    );
   }
+
 }
